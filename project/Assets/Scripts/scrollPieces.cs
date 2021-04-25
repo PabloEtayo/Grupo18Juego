47c58b4c668cd82fa57d8b3ec71a56157adde9d9
Alfonso Rodulfo <arodulfo@ucm.es>
2021-04-25 19:39:52 +0200

    Creada la funcionalidad para rotar piezas

---
+++ project/Assets/Scripts/scrollPieces.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollPieces : MonoBehaviour
+{
+    // Este script permite situar las piezas con el ratón en la posición correcta del tablero.
+}
